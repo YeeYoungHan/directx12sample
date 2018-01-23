84d407fecf9f309b2ccb97eb2486e2393f429263
Yee Young Han <websearch@naver.com>
2018-01-23 11:16:27 +0900

    DirectX12 로 삼각형 그리기 샘플 프로그램 개발 시작

---
+++ TestTriangle/CDirectXTriangle.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CDirectXTriangle.h"
+
+
+CDirectXTriangle::CDirectXTriangle()
+{
+}
+
+
+CDirectXTriangle::~CDirectXTriangle()
+{
+}
---
+++ TestTriangle/CDirectXTriangle.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CDirectX12.h"
+
+class CDirectXTriangle : public CDirectX12
+{
+public:
+	CDirectXTriangle();
+	virtual ~CDirectXTriangle();
+};
+
